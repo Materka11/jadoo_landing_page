6321cac0d9a99dd5c3dd9a312f69a25e92327be9
Materka11 <arekmaterka11@gmail.com>
2025-01-24 14:15:19 +0100

    Refactor: Update Tailwind CSS integration and font variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,6 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    fontFamily: {
-      opensans: ['"Open Sans"', 'sans-serif'],
-      popins: ['"Poppins"', 'sans-serif'],
-      volkov: ['"Volkov"', 'sans-serif'],
-      montserrat: ['"Montserrat"', 'sans-serif'],
-    },
     extend: {},
   },
   plugins: [],
