c5875c3ebcd35712b8b9c582e8595bd74881240a
Adrianm7777 <Adrianmaterka10@gmail.com>
2025-01-27 14:20:05 +0100

    Fix: Correct default font variable syntax and add ContentText component with stories

---
+++ src/components/atoms/ContentText.tsx
@@ -0,0 +1,24 @@
+import TextRectangle from '../../assets/TextRectangle';
+
+const ContentText = () => (
+  <div className="flex-col">
+    <div className="matrix-[1,0,0,1,0,0] absolute top-[118.61px] left-[254.16px] z-[-1] h-[66.18px] w-0 transform bg-[#DF6951]">
+      <TextRectangle />
+    </div>
+    <h1 className="font-popins text-xl font-bold text-[#DF6951]">
+      BEST DESTINATIONS AROUND THE WORLD
+    </h1>
+    <h2 className="font-volkov text-[84px] font-bold text-[#181E4B]">
+      Travel, enjoy
+      <br /> and live a new
+      <br /> and full life
+    </h2>
+    <p className="font-popins text-base font-medium text-[#5E6282]">
+      Built Wicket longer admire do barton vanity itself do in it.
+      <br /> Preferred to sportsmen it engrossed listening. Park gate sell they
+      west hard for the.
+    </p>
+  </div>
+);
+
+export default ContentText;
