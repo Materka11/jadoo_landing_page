a5ea9e41974008778f4697ea2465a2637d1fe1be
Materka11 <arekmaterka11@gmail.com>
2025-01-30 14:26:18 +0100

    Feature: Implement Hamburger menu functionality and update Header layout

--- src/components/atoms/Logo.tsx
+++ src/components/atoms/Logo.tsx
@@ -3,7 +3,7 @@ import LogoIMG from '../../assets/image/logo.webp';
 export const Logo = () => {
   return (
     <a aria-label="Home" href="/">
-      <img src={LogoIMG} alt="Logo" />
+      <img src={LogoIMG} alt="Logo" className="w-28 max-w-max" />
     </a>
   );
 };
--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -1,11 +1,16 @@
 import { AuthButton } from '../atoms/AuthButton';
+import { HamburgerIcon } from '../atoms/HamburgerIcon';
 import { LanguageSelector } from '../atoms/LanguageSelector';
 import { Logo } from '../atoms/Logo';
 import { Nav } from '../molecules/Nav';
 import { useTranslation } from 'react-i18next';
+import { useHamburgerStore } from '../../stores/hamburgerStore';
 
 export const Header = () => {
   const { t } = useTranslation();
+  const toggleHamburgerMenu = useHamburgerStore(
+    (state) => state.toggleHamburgerMenu
+  );
 
   const NAV_ITEMS = [
     { label: t('destinations'), link: '/' },
@@ -13,23 +18,29 @@ export const Header = () => {
     { label: t('flights'), link: '/' },
     { label: t('bookings'), link: '/' },
   ];
-
   const LANGUAGES = ['EN', 'PL'];
 
+  const handleHamburgerClick = () => {
+    toggleHamburgerMenu();
+  };
+
   return (
-    <header className="mx-36 my-12 flex h-10 w-auto items-center justify-between">
+    <header className="flex w-auto items-center justify-between p-6 lg:p-0 lg:px-36 lg:py-12">
       <Logo />
-      <Nav items={NAV_ITEMS} />
-      <section className="flex gap-11">
-        <AuthButton label={t('login')} action={() => {}} />
-        <AuthButton
-          label={t('signup')}
-          action={() => {}}
-          isBorder
-          className="ml-2"
-        />
-        <LanguageSelector languages={LANGUAGES} />
-      </section>
+      <div className="absolute top-10 -right-45 flex w-fit flex-col justify-between text-white lg:flex-row lg:items-center lg:gap-20">
+        <Nav items={NAV_ITEMS} />
+        <section className="flex flex-col lg:flex-row lg:gap-11">
+          <AuthButton label={t('login')} action={() => {}} />
+          <AuthButton
+            label={t('signup')}
+            action={() => {}}
+            isBorder
+            className="ml-2"
+          />
+          <LanguageSelector languages={LANGUAGES} />
+        </section>
+      </div>
+      <HamburgerIcon className="lg:hidden" onClick={handleHamburgerClick} />
     </header>
   );
 };
