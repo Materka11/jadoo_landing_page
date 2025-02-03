d2dd617e5ee626967565392d4062fd768d3ac0e9
Materka11 <arekmaterka11@gmail.com>
2025-02-03 13:16:07 +0100

    Refactor: Update labelStyles in Category component for improved font weight

--- src/components/organisms/Category.tsx
+++ src/components/organisms/Category.tsx
@@ -6,7 +6,7 @@ export const Category = () => {
       <Headers
         label="Category"
         sublabel="We Offer Best Services"
-        labelStyles="text-sm lg:text-lg text-gray-600 font-bold uppercase"
+        labelStyles="text-sm lg:text-lg text-gray-600 font-[600] uppercase"
         sublabelStyles="text-3xl lg:text-5xl text-navyblue font-bold font-volkhov"
         sectionStyles="gap-1 lg:gap-2 text-center"
       />
