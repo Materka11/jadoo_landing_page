06ad03d6ddf1ea715cfbe5808712c9cd3252c78e
Adrianm7777 <Adrianmaterka10@gmail.com>
2025-02-04 16:24:37 +0100

    Feature: Removed Button and Card components, introduce TopSellingText component

---
+++ src/components/atoms/TopSellingText.tsx
@@ -0,0 +1,12 @@
+export const TopSellingText = () => {
+  return (
+    <div className="flex-col text-center">
+      <h3 className="font-popins text-blueGray text-lg font-semibold">
+        Top Selling
+      </h3>
+      <h2 className="font-volkhov text-navyBlue text-5xl font-bold">
+        Top Destinations
+      </h2>
+    </div>
+  );
+};
