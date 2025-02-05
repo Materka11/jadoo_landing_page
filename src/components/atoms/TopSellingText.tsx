ff224b17b81284fc4a13baa4310a16e4d79ee5f4
Adrianm7777 <Adrianmaterka10@gmail.com>
2025-02-05 17:26:08 +0100

    Refactor TopSellingText component to use Headers for improved structure and styling

--- src/components/atoms/TopSellingText.tsx
+++ src/components/atoms/TopSellingText.tsx
@@ -1,12 +1,13 @@
+import { Headers } from './Headers';
+
 export const TopSellingText = () => {
   return (
-    <div className="flex-col text-center">
-      <h3 className="font-popins text-blueGray text-lg font-semibold">
-        Top Selling
-      </h3>
-      <h2 className="font-volkhov text-navyBlue text-5xl font-bold">
-        Top Destinations
-      </h2>
-    </div>
+    <Headers
+      sectionStyles="text-center"
+      label="Top Selling"
+      labelStyles="font-popins text-blueGray text-lg font-semibold"
+      sublabel="Top Destinations"
+      sublabelStyles="font-volkhov text-navyBlue text-5xl font-bold"
+    />
   );
 };
