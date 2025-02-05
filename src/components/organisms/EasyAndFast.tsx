a42f688b893bf1758bb8a605bbd2186c621e9a8a
Materka11 <arekmaterka11@gmail.com>
2025-02-05 14:10:24 +0100

    Feature: Enhance EasyAndFast component and update HomeTemplate layout

--- src/components/organisms/EasyAndFast.tsx
+++ src/components/organisms/EasyAndFast.tsx
@@ -1,3 +1,15 @@
+import { Headers } from '../atoms/Headers';
+
 export const EasyAndFast = () => {
-  return <section></section>;
+  return (
+    <section>
+      <Headers
+        label={'Easy and Fast'}
+        sublabel={'Book Your Next Trip In 3 Easy Steps'}
+        labelStyles={'font-[600] text-lg text-text-color'}
+        sublabelStyles={'font-bold text-5xl text-deepblue font-volkhov'}
+        sectionStyles="max-w-[500px] gap-4"
+      />
+    </section>
+  );
 };
--- src/components/organisms/MainContent.tsx
+++ src/components/organisms/MainContent.tsx
@@ -3,7 +3,7 @@ import { MainContentImage } from '../atoms/MainContentImage';
 
 const MainContent = () => {
   return (
-    <section className="m-8 flex sm:flex-col sm:items-center sm:justify-center lg:flex-row xl:flex-row 2xl:flex-row">
+    <section className="flex sm:flex-col sm:items-center sm:justify-center lg:flex-row xl:flex-row 2xl:flex-row">
       <TextAndButtons />
       <MainContentImage />
     </section>
