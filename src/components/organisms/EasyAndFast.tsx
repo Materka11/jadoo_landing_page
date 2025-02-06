6e56bb084158cd544195062a4710398081873d51
Materka11 <arekmaterka11@gmail.com>
2025-02-06 12:59:02 +0100

    Feature: Add new icon components and integrate TripPost into EasyAndFast

--- src/components/organisms/EasyAndFast.tsx
+++ src/components/organisms/EasyAndFast.tsx
@@ -1,6 +1,13 @@
 import { Headers } from '../atoms/Headers';
+import { TripPost } from '../atoms/TripPost';
+import PostImage from '../../assets/image/PostImage.webp';
+import { LeafIcon } from '../atoms/LeafIcon';
+import { MapIcon } from '../atoms/MapIcon';
+import { SendIcon } from '../atoms/SendIcon';
 
 export const EasyAndFast = () => {
+  const OPTIONS = [LeafIcon, MapIcon, SendIcon];
+
   return (
     <section>
       <Headers
@@ -9,6 +16,13 @@ export const EasyAndFast = () => {
         labelStyles={'font-[600] text-lg text-text-color'}
         sublabelStyles={'font-bold text-5xl text-deepblue font-volkhov'}
         sectionStyles="max-w-[500px] gap-4"
+      />
+      <TripPost
+        description="14 - 29 June | by Robbin Johnosson"
+        title="Trip To Greece"
+        peopleNum={24}
+        photo={PostImage}
+        options={OPTIONS}
       />
     </section>
   );
