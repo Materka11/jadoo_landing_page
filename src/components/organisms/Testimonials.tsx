aee37e8363bde5706fc984ea6ee4e23a0bf5e110
Materka11 <arekmaterka11@gmail.com>
2025-02-10 13:00:06 +0100

    Feature: Add Avatar image and implement Testimonial component with sample data

--- src/components/organisms/Testimonials.tsx
+++ src/components/organisms/Testimonials.tsx
@@ -1,3 +1,17 @@
+import { ITestimonial, Testimonial } from '../atoms/Testimonial';
+import Avatar from '../../assets/image/Avatar.webp';
+
 export const Testimonials = () => {
-  return <section></section>;
+  const TESTIMONIAL: ITestimonial = {
+    avatar: Avatar,
+    content:
+      '“On the Windows talking painted pasture yet its express parties use. Sure last upon he same as knew next. Of believed or diverted no.”',
+    name: 'Mike Taylor',
+    subname: 'Lahore, Pakistan',
+  };
+  return (
+    <section>
+      <Testimonial {...TESTIMONIAL} />
+    </section>
+  );
 };
