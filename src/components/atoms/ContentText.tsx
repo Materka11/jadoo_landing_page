f05b7bf14813ba55e9c186e034ba6fd80efbdbf6
Adrianm7777 <Adrianmaterka10@gmail.com>
2025-02-17 17:32:21 +0100

    Added translations for new features and updated components to use i18n for dynamic text rendering

--- src/components/atoms/ContentText.tsx
+++ src/components/atoms/ContentText.tsx
@@ -1,20 +1,22 @@
-import { TextRectangle } from './TextRectangle';
+import { useTranslation } from 'react-i18next';
+import TextRectangle from './TextRectangle';
 
-export const ContentText = () => (
-  <div className="relative flex flex-col">
-    <div className="matrix-[1,0,0,1,0,0] bg-orange absolute top-28 right-0 z-1 hidden w-0 transform lg:top-22 lg:block lg:h-16">
-      <TextRectangle className="h-3 w-[376px] -translate-x-[70%]" />
+export const ContentText = () => {
+  const { t } = useTranslation();
+  return (
+    <div className="relative flex flex-col sm:text-center">
+      <div className="matrix-[1,0,0,1,0,0] bg-orange absolute top-28 left-80 z-1 hidden w-0 transform lg:top-22 lg:left-40 lg:block lg:h-16 xl:left-70 2xl:left-70">
+        <TextRectangle />
+      </div>
+      <h2 className="font-popins text-orange mb-3 text-xl font-bold">
+        {t('best_destinations')}
+      </h2>
+      <h1 className="font-volkhov text-content-title text-6xl font-bold lg:text-6xl">
+        {t('travel_enjoy')}
+      </h1>
+      <p className="font-popins text-text-color mt-4 text-base font-medium">
+        {t('description')}
+      </p>
     </div>
-    <h2 className="font-popins text-orange mb-3 text-xl font-bold">
-      BEST DESTINATIONS AROUND THE WORLD
-    </h2>
-    <h1 className="font-volkhov text-content-title text-6xl font-bold lg:text-6xl">
-      Travel, enjoy
-      <br /> and live a new and full life
-    </h1>
-    <p className="font-popins text-text-color mt-4 text-base font-medium">
-      Built Wicket longer admire do barton vanity itself do in it. Preferred to
-      sportsmen it engrossed listening. Park gate sell they west hard for the.
-    </p>
-  </div>
-);
+  );
+};
